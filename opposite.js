45e20a3398e612e5e76a6a220d2c98cebc44d204
midCenturyDude7 <mgriffes@gmail.com>
2025-01-31 08:54:14 -0800

    Simple integer inversion function

---
+++ opposite.js
@@ -0,0 +1,7 @@
+/*
+Given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+*/
+
+const opposite = (number) => -number;
+
+console.log(opposite(1)); // -1
