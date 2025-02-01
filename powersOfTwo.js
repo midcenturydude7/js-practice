4a9bd9465101f21c394e2c0917bfca02ff5f2cd5
midCenturyDude7 <mgriffes@gmail.com>
2025-02-01 10:22:11 -0800

    Updated with customer range method using Array.from

--- powersOfTwo.js
+++ powersOfTwo.js
@@ -2,12 +2,17 @@
 Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive).
 */
 
-const powersOfTwo = (n) => {
+// Using for loop
+const powersOfTwo2 = (n) => {
   const result = [];
   for (let i = 0; i <= n; i++) {
     result.push(2 ** i);
   }
-  return result;
+};
+
+// Working with Array.from method to create a range method
+const powersOfTwo = (n) => {
+  return Array.from({ length: n + 1 }, (_, i) => 2 ** i);
 };
 
 console.log(powersOfTwo(0)); // [1]
