bea31924903be09e6900d85a263a89b291ef600a
midCenturyDude7 <mgriffes@gmail.com>
2025-02-17 11:53:41 -0800

    Working with strings along with different methods to dynamically change a letter's case

---
+++ camelCase.js
@@ -0,0 +1,45 @@
+/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+*/
+
+// Regular Expression Solution
+const camelCase = (str) => {
+  return str.replace(/([A-Z])/g, " $1");
+};
+
+// For Loop Solution
+const camelCase2 = (str) => {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      result += " " + str[i];
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+};
+
+// Split and Join Solution
+const camelCase3 = (str) => {
+  return str.split(/(?=[A-Z])/).join(" ");
+};
+
+// Split and Join without Regex Solution
+const camelCase4 = (str) => {
+  return str
+    .split("")
+    .map((char) => {
+      if (char === char.toUpperCase()) {
+        return " " + char;
+      } else {
+        return char;
+      }
+    })
+    .join("");
+};
+
+console.log(camelCase("camelCasing")); // camel Casing
+console.log(camelCase2("camelCasing")); // camel Casing
+console.log(camelCase3("camelCasing")); // camel Casing
+console.log(camelCase4("camelCasing")); // camel Casing
