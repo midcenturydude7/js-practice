4dec3802af6223144deaa8356b8ca5dcdbd8a04d
midCenturyDude7 <mgriffes@gmail.com>
2025-02-19 18:48:12 -0800

    Used parseInt() to return number of a string of letters

--- parseIntFromChar.js
+++ parseIntFromChar.js
@@ -5,3 +5,9 @@ Write a program that returns the girl's age (0-9) as an integer.
 
 Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
 */
+
+const getAge = (inputString) => {
+  return parseInt(inputString);
+};
+
+console.log(getAge("4 years old")); // 4
